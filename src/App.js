554b7d30f82dd69ca22341df0a81f388c80af45d
bagusnata330 <bagusnata330@gmail.com>
2020-05-10 22:01:32 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       isloading: false
+    }
+  }
+
+  async componentDidMount(){
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log("data", data)
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>sadksan</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
