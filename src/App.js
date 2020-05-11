f37a8f10c84ec8e0f50306b118157e8926f49b99
bagusnata330 <bagusnata330@gmail.com>
2020-05-11 22:02:13 +0800

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./App.css";
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,16 +8,24 @@ import {
 } from "react-router-dom";
 import Home from './pages/home';
 import About from './pages/about';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-      </Switch>
+      <div className="container">
+        <div className="row">
+          <Navbar />
+        </div>
+        <div className="row margin-top-large">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </div>
     </Router>
-  )
+  );
 }
 
 export default App
