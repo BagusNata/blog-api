3b1f6c61a2ee6fdd461fa2e17d3d97ee6ed5db90
bagusnata330 <bagusnata330@gmail.com>
2020-05-13 20:44:51 +0800

    add data but can't display it

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  
 } from "react-router-dom";
 import Home from './pages/home';
 import About from './pages/about';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 
-function App() {
+
+function App() { 
   return (
     <Router>
       <div className="container">
         <div className="row">
           <Navbar />
         </div>
-        <div className="row margin-top-large">
-          <Switch>
+        <div>
+          <Switch >
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
           </Switch>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+
 
 export class home extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Home page</h1>
-      </div>
-    )
-  }
-}
+         constructor() {
+           super();
+           this.state = {
+             isLoading: true,
+             person: null
+           };
+         }
 
-export default home
+        async componentDidMount(){
+          const url = "https://api.randomuser.me/";
+          const response = await fetch(url);
+          const data = await response.json();
+          this.setState({person: data.results[0], isLoading:false})
+
+          console.log("data :", this.state.person)
+
+        }
+
+
+         render() {
+           if (this.state.isLoading) {
+             return <div className="header">loading...</div>;
+           };
+
+           if (!this.state.isLoading) {
+             return <div className="header">didn't get person</div>;
+           };
+
+           return (
+             <div>
+               <div>
+                 <div>{this.state.person.name.title}</div>
+                 <div>{this.state.person.name.first}</div>
+                 <div>{this.state.person.name.last}</div>
+                 <img src={this.state.person.picture.large} />
+               </div>
+             </div>
+           );
+         }
+       }
+
+export default home;
