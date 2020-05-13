afde55de1f58f426da2838110e0d34d041d2f0f6
vedanta <vedanta.maha@zettabyte.sg>
2020-05-13 22:09:05 +0800

    show posts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-      </Switch>
-    </Router>
-  )
+    <div className='container'>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getPostData();
+  }
+
+  async getPostData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    this.setState({ posts: data });
+  }
+
+  getPostCards() {
+    const data = [];
+    this.state.posts.forEach((user, index) => {
+      data.push(
+        <div key={index} className='card mb-3'>
+          <div className='card-body'>
+            <h5 className='card-title'>{user.title}</h5>
+            <p className='card-text'>{user.body}</p>
+            <button type="button" className="btn btn-primary">Read more</button>
+          </div>
+        </div>
+      );
+    });
+    return data;
+  }
+
   render() {
+    const postsCardJsx = this.getPostCards();
     return (
       <div>
-        <h1>Home page</h1>
+        <h1 className='my-5 text-center'>Home page</h1>
+        {postsCardJsx}
       </div>
-    )
+    );
   }
 }
 
-export default home
+export default home;
